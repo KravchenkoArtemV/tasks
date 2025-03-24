f1415222a89921afd4efd8c3031fe868b0def045
KravchenkoArtemV <artem-k-art@yandex.ru>
2025-03-24 11:43:25 +0300

    add concur task 1

---
+++ concurency/write_and_read_two_goroutines.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// задача на отправку из одной горутины данных и чтения в другой
+
+func main() {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		ch <- 3
+		ch <- 7
+		ch <- 11
+		close(ch) // закрывает канал отправитель
+
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := range ch {
+			fmt.Println(i)
+		}
+	}()
+
+	wg.Wait()
+
+}
