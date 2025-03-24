371401d765a58d5e4e8c59715838ce625bcec5f5
KravchenkoArtemV <artem-k-art@yandex.ru>
2025-03-25 00:52:03 +0300

    add concur mutex task

---
+++ concurency/mutex_and_waitgroup.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var mu sync.Mutex
+	counter := 0
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				mu.Lock()   // захватываем доступ
+				counter++   // безопасно увеличиваем
+				mu.Unlock() // освобождаем доступ
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Итоговое значение:", counter) // ожидается 5000
+}
